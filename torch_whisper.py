bec5c2cb9df4e057cb43bdf0b53be551079fdf7a
roryclear <roryclear.rc@gmail.com>
2025-02-25 15:35:54 +0000

    add assert

--- torch_whisper.py
+++ torch_whisper.py
@@ -1,5 +1,12 @@
 import whisper
+import time
+st = time.perf_counter()
 model = whisper.load_model("tiny")
 result = model.transcribe("TINYCORP_MEETING_2025-01-27.mp3")
 print(result["text"])
-print(type(result["text"]),type(result))
+print("\ntime taken: {:.2f}".format(time.perf_counter() - st))
+
+with open('torch_output.txt', 'r') as file:
+    file_content = file.read()
+
+assert(result["text"] == file_content)
