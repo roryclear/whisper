d23ef2b39b8ab0dd3ad26e838d704b6afc8c0bb6
roryclear <roryclear.rc@gmail.com>
2025-02-27 10:38:19 +0000

    start pad_or_trim thing

--- tiny_whisper.py
+++ tiny_whisper.py
@@ -948,30 +948,30 @@ TOKENS_PER_SECOND = exact_div(SAMPLE_RATE, N_SAMPLES_PER_TOKEN)  # 20ms per audi
 
 __version__ = "20240930"
 
+def index_select(input_tensor, dim, index):
+    if type(input_tensor) == Tensor: input_tensor = tiny_Tensor(input_tensor.numpy())
+    expanded_index = [slice(None)] * len(input_tensor.shape)
+    expanded_index[dim] = index
+    input_tensor = input_tensor.numpy()
+    ret = input_tensor[tuple(expanded_index)] #todo, np
+    return Tensor(ret)
+
 def pad_or_trim(array, length: int = N_SAMPLES, *, axis: int = -1):
     """
     Pad or trim the audio array to N_SAMPLES, as expected by the encoder.
     """
-    if torch.is_tensor(array):
-        if array.shape[axis] > length:
-            array = array.index_select(
-                dim=axis, index=torch.arange(length, device=array.device)
-            )
+    if array.shape[axis] > length:
+        y = tiny_Tensor.arange(length,dtype=dtypes.int32)
+        y = Tensor(y.numpy()).to(dtype=torch.int32)
+        array = tiny_Tensor(array.numpy())
+        array = index_select(array, dim=axis, index=y)
 
-        if array.shape[axis] < length:
-            pad_widths = [(0, 0)] * array.ndim
-            pad_widths[axis] = (0, length - array.shape[axis])
-            array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes])
-    else:
-        if array.shape[axis] > length:
-            array = array.take(indices=range(length), axis=axis)
-
-        if array.shape[axis] < length:
-            pad_widths = [(0, 0)] * array.ndim
-            pad_widths[axis] = (0, length - array.shape[axis])
-            array = np.pad(array, pad_widths)
-
+    if array.shape[axis] < length:
+        pad_widths = [(0, 0)] * array.ndim
+        pad_widths[axis] = (0, length - array.shape[axis])
+        array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes])
     return array
+
 
 @lru_cache(maxsize=None)
 def get_encoding(name: str = "gpt2", num_languages: int = 99):
